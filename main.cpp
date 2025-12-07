c089fb162f31c9a4b894af4ffb2fe90044288f12
Hemal Patel <95436169+HemalSPatel@users.noreply.github.com>
2025-12-07 18:20:55 -0500

    feat: guardrails

--- main.cpp
+++ main.cpp
@@ -46,7 +46,7 @@ void toggleTaskStatus(int taskNumber, std::vector<Task>& tasks) {
 void deleteTask(int taskNumber, std::vector<Task>& tasks)
 {
     taskNumber -= 1;
-    if (taskNumber >= 0 && taskNumber < (int)tasks.size())
+    if (taskNumber >= 0 && taskNumber <static_cast<int>(tasks.size()))
     {
         std::swap(tasks[taskNumber], tasks.back());
         tasks.pop_back();
@@ -69,26 +69,50 @@ int main()
         char command;
         std::cout << "Choose command (a - add, v - view all, t - toggle task completion, d - delete task, q - quit): " << " ";
         std::cin >> command;
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
         switch (command) {
         case 'a':
             std::cout << "Adding task..." << std::endl;
             addTask(tasks);
             break;
-        case 'v':
-            std::cout << "Viewing tasks..." << std::endl;
-            viewTasks(tasks);
-            break;
         case 't':
             int updateNumber;
-            std::cout << "Enter the task number you would like to toggle the completion status for: ";
-            std::cin >> updateNumber;
+            bool validUpdateInput;
+            validUpdateInput = false;
+            do {
+                std::cout << "Enter the task number you would like to toggle the completion status for: ";
+                std::cin >> updateNumber;
+                if (std::cin.fail()) {
+                    std::cout << "Invalid input. Please enter a integer!" << std::endl;
+                    std::cin.clear();
+                    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                } else {
+                    validUpdateInput = true;
+                }
+            } while (!validUpdateInput);
             toggleTaskStatus(updateNumber, tasks);
             break;
         case 'd':
             int deleteNumber;
-            std::cout << "Enter the task number you would like to delete: ";
-            std::cin >> deleteNumber;
+            bool validDeleteInput;
+            validDeleteInput = false;
+            do {
+                std::cout << "Enter the task number you would like to delete: ";
+                std::cin >> deleteNumber;
+                if (std::cin.fail()) {
+                    std::cout << "Invalid input. Please enter a integer!" << std::endl;
+                    std::cin.clear();
+                    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                } else
+                {
+                    validDeleteInput = true;
+                }
+            } while (!validDeleteInput);
             deleteTask(deleteNumber, tasks);
+            break;
+        case 'v':
+            std::cout << "Viewing tasks..." << std::endl;
+            viewTasks(tasks);
             break;
         case 'q':
             std::cout << "Quitting..." << std::endl;
